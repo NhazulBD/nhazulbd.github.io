fcbd4d8efc1b4ded6690d1eaa76007fc7cfd0f5c
Pedro Enrique Ramos Zafra <pedrinchi1999@gmail.com>
2021-07-15 17:42:21 -0400

    K4U - MC R2

---
+++ projects/kiss4u/src/js/model-wanted-company.js
@@ -0,0 +1,21 @@
+/*
+  [JS Index - Model Center / Company]
+  
+  ---
+  
+  Author:   RZ
+*/
+
+/*
+
+*/
+
+$(function() {
+    "use strict";
+    $(document).ready(function() {
+        setTimeout(function(){ 
+        }, 2000);
+    });
+
+
+});
---
+++ projects/kiss4u/src/js/model-wanted-single.js
@@ -0,0 +1,21 @@
+/*
+  [JS Index - Model Center / Single]
+  
+  ---
+  
+  Author:   RZ
+*/
+
+/*
+
+*/
+
+$(function() {
+    "use strict";
+    $(document).ready(function() {
+        setTimeout(function(){ 
+        }, 2000);
+    });
+
+
+});

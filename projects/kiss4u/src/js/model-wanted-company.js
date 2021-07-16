c2f185483f24dd3c5692762e54bb2e5a6967dc02
Pedro Enrique Ramos Zafra <pedrinchi1999@gmail.com>
2021-07-16 16:55:12 -0400

    K4U - Model Wanted Company  P1

--- projects/kiss4u/src/js/model-wanted-company.js
+++ projects/kiss4u/src/js/model-wanted-company.js
@@ -1,8 +1,8 @@
 /*
   [JS Index - Model Center / Company]
-  
+
   ---
-  
+
   Author:   RZ
 */
 
@@ -11,11 +11,47 @@
 */
 
 $(function() {
-    "use strict";
-    $(document).ready(function() {
-        setTimeout(function(){ 
-        }, 2000);
-    });
+  "use strict";
+  $(document).ready(function() {
+      setTimeout(function(){ 
+      }, 2000);
+  });
+
+  $("#categoryNameWrapper").hide();
+  $("#selectCategory").hide();
+
+  $("#categoryWrapper a").click( function (){
+    var addCheck =  $(this).find("input");
+    $(addCheck).click();
+    
+    var activeCard = $(this).attr("id");
+
+    $(".typeCard").removeClass("active");
+    $(".typeCard[id='card"+activeCard+"']").addClass("active");
+  });
+
+
+  $(".btn-nextpill").click( function(){
+    var btnID = $(this).attr("category");
+    $(".btn-pill[id='"+btnID+"']").click();
+  });
+
+$("input[type=file]").change(function (){
+  var file = $(this).get(0).files[0];
+
+  var elImg = $(this).parent().parent().parent().parent().parent().find("img.card-img-top");
+
+  if(file){
+    var reader = new FileReader();
+
+    reader.onload = function(){
+        $(elImg).attr("src", reader.result);
+    }
+
+    reader.readAsDataURL(file);
+  }
+
+});
 
 
 });
--- projects/kiss4u/src/js/model-wanted-single.js
+++ projects/kiss4u/src/js/model-wanted-single.js
@@ -34,8 +34,28 @@ $(function() {
       }
     });
 
-    $("#dateOfBirth").click( function(){
-      
+
+    $(".btn-nextpill").click( function(){
+      var btnID = $(this).attr("category");
+      $(".btn-pill[id='"+btnID+"']").click();
     });
+
+  $("input[type=file]").change(function (){
+    var file = $(this).get(0).files[0];
+
+    var elImg = $(this).parent().parent().parent().parent().parent().find("img.card-img-top");
+
+    if(file){
+      var reader = new FileReader();
+
+      reader.onload = function(){
+          $(elImg).attr("src", reader.result);
+      }
+
+      reader.readAsDataURL(file);
+    }
+
+  });
+
 
 });
